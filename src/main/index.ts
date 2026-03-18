7d6c46f3c219c76c22bd39000a555b81d73cc968
banu <babajanovabenazir2004@gmail.com>
2026-03-19 03:22:02 +0800

    added auto-updater

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,37 @@
 import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
+import { autoUpdater } from 'electron-updater'
+import { dialog } from 'electron'
 import icon from '../../resources/icon.png?asset'
+
+autoUpdater.autoDownload = false
+
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Available',
+    message: 'A new version of SnackStack is available. Would you like to download it?',
+    buttons: ['Download', 'Later']
+  }).then(result => {
+    if (result.response === 0) autoUpdater.downloadUpdate()
+  })
+})
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'Update Ready',
+    message: 'Update downloaded. SnackStack will restart to install the update.',
+    buttons: ['Restart Now']
+  }).then(() => {
+    autoUpdater.quitAndInstall()
+  })
+})
+
+autoUpdater.on('error', (err) => {
+  dialog.showErrorBox('Update Error', err.message)
+})
 
 function createWindow(): void {
   // Create the browser window.
@@ -35,16 +65,10 @@ function createWindow(): void {
   }
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
   app.on('browser-window-created', (_, window) => {
     optimizer.watchWindowShortcuts(window)
   })
@@ -54,21 +78,15 @@ app.whenReady().then(() => {
 
   createWindow()
 
+  autoUpdater.checkForUpdatesAndNotify()
+
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
